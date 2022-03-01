7b4e33e24d4d6c5b2339f095352e9060ada1821f
Thijs Creemers <thijs.creemers@gmail.com>
2022-03-01 07:37:15 +0100

    Fixed some tests and added a README

---
+++ README.md
@@ -0,0 +1,42 @@
+# Scramblies challenge
+
+## Prerequisites
+```bash
+brew install clojure yarn  # installs the clojure and yarn packages
+yarn install               # installs all npm dependencies
+```
+## Install environment
+
+```bash
+git clone git@github.com:thijs-creemers/scrambles.git
+
+cd scrambles
+yarn clean
+```
+This installs a clean set of node npm modules and compiles the css.
+
+## Run unit-tests
+
+clj -X:test
+
+## Start the api-server for development
+- Start and connect to the REPL.
+- load the namespace scrambles.api-server.
+- start the server with (-main)
+- The server can be stopped wit (stop-server)
+
+## Compile and run the dev server for the frontend
+```bash
+yarn dev
+```
+Wait a few moments for the compilation to complete and go the your browser and open
+the app with the url: 'http://127.0.0.1:8101'.
+
+## Create a optimized runtime version of the frontend app 
+```bash
+yarn release
+```
+
+## To be done
+- create and test build env for the api-server
+- more tests
--- test/scrambles/api_server_test.clj
+++ test/scrambles/api_server_test.clj
@@ -15,24 +15,19 @@
                     (mock/json-body {:letters "vueaeacelue" :word "value"})))
            {:status  201
             :headers {"Content-Type" "application/json; charset=utf-8"}
-            :body    "{\"status\":\"success\",\"result\":true,\"params\":[\"vueaeacelue\",\"value\"]}"})))
+            :body    "{\"status\":\"success\",\"result\":true,\"message\":\"\",\"params\":[\"vueaeacelue\",\"value\"]}"})))
 
   (testing "Happy path for /api/v1/scramble, false returned"
     (is (= (app (-> (mock/request :post "/api/v1/scramble")
                     (mock/json-body {:letters "lvowrl" :word "world"})))
            {:status  201
             :headers {"Content-Type" "application/json; charset=utf-8"}
-            :body     "{\"status\":\"success\",\"result\":false,\"params\":[\"lvowrl\",\"world\"]}"})))
+            :body    "{\"status\":\"success\",\"result\":false,\"message\":\"\",\"params\":[\"lvowrl\",\"world\"]}"})))
 
   (testing "Invalid user input"
     (is (= (app (-> (mock/request :post "/api/v1/scramble")
-                    (mock/json-body {:letters "vueaeacelue" :word "val3ue"})))
-           {:status  400
-            :headers {"Content-Type" "application/json; charset=utf-8"}
-            :body    "{\"status\":\"failed\",\"message\":\"Both the letters and word strings must be in lowercase characters.\",\"params\":[\"vueaeacelue\",\"val3ue\"]}"}))
+                    (mock/json-body {:letters "vueaeacelue" :word "val3ue"}))))
 
-    (is (= (app (-> (mock/request :post "/api/v1/scramble")
-                    (mock/json-body {:letters "vueaeacelue" :word "vaLUe"})))
-           {:status  400
-            :headers {"Content-Type" "application/json; charset=utf-8"}
-            :body    "{\"status\":\"failed\",\"message\":\"Both the letters and word strings must be in lowercase characters.\",\"params\":[\"vueaeacelue\",\"vaLUe\"]}"}))))
+        {:body    "{\"status\":\"failed\",\"result\":false,\"message\":\"Both the letters and word strings must be in lowercase characters.\",\"params\":[\"vueaeacelue\",\"vaLUe\"]}"
+         :headers {"Content-Type" "application/json; charset=utf-8"}
+         :status  400})))
